b48fd1e82c07454f6e94bcb6a682e61f4f5a3444
Ryo_Odo <marukae@gmail.com>
2021-11-25 02:03:19 +0000

    新規作成後、詳細画面へ飛ぶように修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(tasks_params)
     if @task.save
-      redirect_to tasks_path, notice: "登録しました！"
+      redirect_to task_path(@task.id), notice: "登録しました！"
     else
       render :new
     end
