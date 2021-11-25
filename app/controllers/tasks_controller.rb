3e31af3853a2ecf799ff3b104a8f7b1036d04f3e
Ryo_Odo <marukae@gmail.com>
2021-11-25 01:57:46 +0000

    TaskのCRUD機能作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to tasks_path, notice: "登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(tasks_params)
+      redirect_to tasks_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"削除しました"
+  end
+
+  private
+  def set_property
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:task_name, :content)
+  end
+end
