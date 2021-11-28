6dfca82969018d24b65985793ca552759c3a238d
Ryo_Odo <marukae@gmail.com>
2021-11-28 04:40:05 +0000

    ユーザーログイン機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_082903) do
+ActiveRecord::Schema.define(version: 2021_11_28_041001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_11_27_082903) do
     t.string "status", default: "完了", null: false
     t.integer "priority", default: 0, null: false
     t.index ["task_name"], name: "index_tasks_on_task_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
